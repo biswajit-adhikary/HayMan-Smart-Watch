1c10297b4ecca2d377bfb8e4d74e0010d6cc13a1
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-04-04 13:11:29 +0600

    404 page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404, Not Found.</h2>
+        <div className='notfound-area'>
+            <div className="theme-container">
+                <h3>404</h3>
+                <h2>Not Found!</h2>
+                <div className="theme-link">
+                    <Link to="/">Back Home</Link>
+                </div>
+            </div>
         </div>
     );
 };
